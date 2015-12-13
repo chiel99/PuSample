337ebdc3b3b409294b8eca952d33dfba709697f1
Richard Chang <chiel99@gmail.com>
2015-12-13 23:54:19 +0800

    [Title]: Print Username on MainActivity
    [Problem]:
    [Solution]:
    [Note]:

--- app/src/main/java/pu/edu/pusample/MainActivity.java
+++ app/src/main/java/pu/edu/pusample/MainActivity.java
@@ -1,16 +1,27 @@
 package pu.edu.pusample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+        String password = intent.getStringExtra("password");
+
+        mTextView = (TextView) findViewById(R.id.text);
+        mTextView.setText("Welcome " + username + "!!!!");
     }
 
     @Override
