f1a1bab9773444521f109ede08bd8125785678d8
Richard Chang <chiel99@gmail.com>
2015-12-13 22:40:50 +0800

    [Title]: Add Login Page
    [Problem]:
    [Solution]:
    [Note]:

---
+++ app/src/main/java/pu/edu/pusample/LoginActivity.java
@@ -0,0 +1,124 @@
+package pu.edu.pusample;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private UserLoginTask mAuthTask = null;
+
+    private EditText mAccountText;
+    private EditText mPasswordText;
+    private Button mLoginButton;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAccountText = (EditText) findViewById(R.id.edit_account);
+        mPasswordText = (EditText) findViewById(R.id.edit_password);
+        mLoginButton = (Button) findViewById(R.id.btn_login);
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Logging in");
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLogin();
+            }
+        });
+    }
+
+    private void startLogin() {
+        String username = mAccountText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            // TODO: handle invalid login input
+            Toast.makeText(this, "Empty inputs", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (mAuthTask != null) {
+            // avoid multiple login task
+            return;
+        }
+
+        showProgress(true);
+        mAuthTask = new UserLoginTask(username, password);
+        mAuthTask.execute((Void) null);
+    }
+
+    private void showProgress(boolean show) {
+        if (mProgressDialog != null) {
+            if (show) {
+                mProgressDialog.show();
+            } else {
+                mProgressDialog.dismiss();
+            }
+        }
+    }
+
+    private void enterMainPage(String username, String password) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+        startActivity(intent);
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+
+        UserLoginTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                enterMainPage(mUsername, mPassword);
+            } else {
+                // TODO: show error message
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+}
